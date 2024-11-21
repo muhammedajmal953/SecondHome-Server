f6c09b8ff9e478dafa2196e5af15509dc9f205db
muhammedajmal953 <muhammedajmal953@gmail.com>
2024-11-21 10:31:54 +0530

    editig profile bug fixing

--- src/app.ts
+++ src/app.ts
@@ -1,45 +1,44 @@
-import express, { Express } from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import userRouter from './routes/userRoutes';
+import express, { Express } from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import userRouter from "./routes/userRoutes";
 
-import venderRouter from './routes/vendorRoutes';
-import adminRouter from './routes/adminRoutes';
-import fs from 'fs'
-import { job } from './utils/cronJob';
-import path from 'path';
-
+import venderRouter from "./routes/vendorRoutes";
+import adminRouter from "./routes/adminRoutes";
+import fs from "fs";
+import { job } from "./utils/cronJob";
+import path from "path";
 
 export const app: Express = express();
 
 dotenv.config();
-job.start()
+job.start();
 
-app.use(express.json({ limit: '10mb' })); 
-app.use(express.urlencoded({ limit: '10mb', extended: true })); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cors({
+app.use(
+  cors({
     origin: process.env.frontEnd_URL,
     credentials: true,
-methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-}))
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization","Accept"],
+  })
+);
 
+const fileName = new Date().toISOString().split("T")[0];
+const loginDirectory = path.join(__dirname, "logs");
+const appLogStream = fs.createWriteStream(
+  path.join(loginDirectory, `${fileName}.log`),
+  { flags: "a" }
+);
 
-const fileName=new Date().toISOString().split('T')[0]
-const loginDirectory=path.join(__dirname,'logs')
-const appLogStream = fs.createWriteStream(path.join(loginDirectory, `${fileName}.log`), { flags: 'a' })
- 
-
-app.use(morgan('combined', { stream: appLogStream }))
+app.use(morgan("combined", { stream: appLogStream }));
 // app.use(morgan('dev'))
-   
-app.use('/',userRouter)
-app.use('/vendor', venderRouter)
-app.use('/admin',adminRouter)  
 
-export default app
+app.use("/", userRouter);
+app.use("/vendor", venderRouter);
+app.use("/admin", adminRouter);
 
-
- 
+export default app;
